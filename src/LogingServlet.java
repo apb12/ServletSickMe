97b51528433ffcab874abbab47de38e56e00395b
apb12 <apbapb12@yahoo.com>
2019-06-25 08:15:12 +0300

    Initial commit

---
+++ src/LogingServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "LogingServlet",urlPatterns = "/login")
+public class LogingServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
